791477215fe3b6f002fc4c35a559cd99df8244a2
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-06-04 08:44:37 -0300

    First commit in Branch dev

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <>
+      <View
+        style={{
+          height: 100,
+          alignItems: "center",
+          paddingHorizontal: 8,
+          paddingVertical: 12,
+        }}
+      >
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+        <Text>Hello!</Text>
+      </View>
+    </>
+  );
+}
